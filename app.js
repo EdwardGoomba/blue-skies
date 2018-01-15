f9c23f6e56204cc5d6356d809d3f049184637403
EdwardGoomba <ask@edwardvdesign.com>
2018-01-14 20:57:01 -0500

    Add function to print weather at users zip

---
+++ app.js
@@ -0,0 +1,22 @@
+// Input zip code and get current weather data
+
+// Import modules
+
+// Store api key
+
+// Get weather and print to console
+
+function printWeather(yourZip) {
+  const weather = `The weather at zip ${yourZip} is `;
+  console.log(weather);
+}
+
+// Connect to API Url
+
+// Read the data
+
+// Parse the data
+
+// Print the data
+
+// Input zip via command line
