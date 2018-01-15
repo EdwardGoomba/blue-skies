c92f0c89b8a2c1a034774285709128668d2409c2
EdwardGoomba <ask@edwardvdesign.com>
2018-01-14 21:39:34 -0500

    Print weather for multiple locations

--- app.js
+++ app.js
@@ -6,34 +6,41 @@ const https = require('https');
 // Store api key
 const apiKey = 'd069855404e65ba709f0867ff8018551';
 
-//Set zip
-const yourZip = '17552';
-
 // Get weather and print to console
 function printWeather(location, conditions) {
   const weather = `The weather in ${location} is ${conditions} Kelvin`;
   console.log(weather);
 }
 
+function getWeather(yourZip) {
 // Connect to API Url
-const request = https.get(`https://api.openweathermap.org/data/2.5/weather?zip=${yourZip}&appid=${apiKey}`, response => {
+  const request = https.get(`https://api.openweathermap.org/data/2.5/weather?zip=${yourZip}&appid=${apiKey}`, response => {
 
-  let body = '';
+    let body = '';
 
-  // Read the data
-  response.on('data', data => {
-    body += data.toString();
+    // Read the data
+    response.on('data', data => {
+      body += data.toString();
+    });
+
+    response.on('end', () => {
+      // Parse the data
+      const location = JSON.parse(body);
+
+      // Print the data
+      printWeather(location.name, location.main.temp);
+
+    })
+
   });
 
-  response.on('end', () => {
-    // Parse the data
-    const location = JSON.parse(body);
+}
 
-    // Print the data
-    printWeather(location.name, location.main.temp);
+// Set zip codes you want to pull weather for
+const locations = ['17552', '98119'];
 
-  })
-
+locations.forEach(yourZip => {
+  getWeather(yourZip);
 });
 
 // Input zip via command line
