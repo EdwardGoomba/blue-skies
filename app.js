79850a49ce7aada353db030a39a0ab19c7ec8b4c
EdwardGoomba <ask@edwardvdesign.com>
2018-01-14 21:01:45 -0500

    Connect to api and verify connection

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 // Input zip code and get current weather data
 
 // Import modules
+const https = require('https');
 
 // Store api key
+const apiKey = 'd069855404e65ba709f0867ff8018551';
+
+const yourZip = '17552';
 
 // Get weather and print to console
-
 function printWeather(yourZip) {
   const weather = `The weather at zip ${yourZip} is `;
   console.log(weather);
 }
 
 // Connect to API Url
+const request = https.get(`https://api.openweathermap.org/data/2.5/weather?zip=${yourZip}&appid=${apiKey}`, response => {
+  console.log(response.statusCode);
 
-// Read the data
+  // Read the data
 
-// Parse the data
+  // Parse the data
 
-// Print the data
+  // Print the data
+
+});
 
 // Input zip via command line
